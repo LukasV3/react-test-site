e0970e1ea7d1fed4b5a2ea91263b6f579af4393e
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-03-11 18:38:12 +0000

    made header__content section responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="header__content">
         <h5 className="header__title">Welcome Heather</h5>
         <p className="header__header">
-          More cash in your wallet<br></br> from everyday shopping
+          More cash in your wallet from everyday shopping
         </p>
         <p className="header__description">
           Shop for things you'd buy anyway with over 3,000 brands - online and
@@ -25,14 +25,6 @@ const Header = () => {
             alt="Arrow right"
           ></img>
         </button>
-
-        {/* <div className="header__img--div">
-          <img
-            className="header__img"
-            src={HeaderImage}
-            alt="User dashboard"
-          ></img>
-        </div> */}
       </div>
     </div>
   )
