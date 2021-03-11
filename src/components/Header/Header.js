78add18a68a38c22351b34feed0c20936c9bf2d5
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-03-11 14:20:24 +0000

    completed majority of header__content styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,32 @@ import React from "react"
 import "./styles.scss"
 
 import HeaderImage from "../../assets/images/header-image.png"
+import ArrowRight from "../../assets/svgs/arrow.svg"
+import Wallet from "../../assets/svgs/wallet.svg"
 
 const Header = () => {
   return (
     <div className="header">
-      <p>More cash in your wallet from everyday shopping</p>
-      <img src={HeaderImage} alt="Site Dashboard"></img>
-      <button>Activate Now</button>
-      <div></div>
+      <div className="header__content">
+        <h5 className="header__title">Welcome Heather</h5>
+        <p className="header__header">
+          More cash in your wallet from everyday shopping
+        </p>
+        <p className="header__description">
+          Shop for things you'd buy anyway with over 3,000 brands - online and
+          in store - and watch the cash stack up in your wallet.
+        </p>
+        <button className="header__btn">
+          <span className="header__btn--text">Activate now</span>
+          <img
+            className="header__btn--icon"
+            src={ArrowRight}
+            alt="Arrow right"
+          ></img>
+        </button>
+      </div>
+
+      <div className="header__progress-bar"></div>
     </div>
   )
 }
